db57727a7db3350779b8a1e4bffffb0df0a47b04
Samuel Wehunt <sam.wehunt@gmail.com>
2018-11-27 13:40:55 -0600

    made it track timestamps per directory

--- c2trash/modules/newFiles.py
+++ c2trash/modules/newFiles.py
@@ -1,9 +1,12 @@
 #Show all files that have been made/changed since the last call
 #only works on linux right now
-SETTINGS["newFilesTimestamp"] = 0
+SETTINGS["newFilesTimestamp"] = {}
 def newFiles(start="/"):
     send_msg("newFiles", "checking {} for new shit".format(start))
     output = []
+    time_stamp = SETTINGS['newFilesTimestamp'].get(start, None)
+    if time_stamp is None:
+        time_stamp = 0
     for root, dirs, files in os.walk(start):
         for name in files:
             f = os.path.join(root, name)
@@ -14,16 +17,16 @@ def newFiles(start="/"):
                 continue
             mask = 0
             #access
-            if stats.st_atime > SETTINGS["newFilesTimestamp"]:
+            if stats.st_atime > time_stamp:
                 mask += 1
             #modify
-            if stats.st_mtime > SETTINGS["newFilesTimestamp"]:
+            if stats.st_mtime > time_stamp:
                 mask += 2
             #metadata change
-            if stats.st_ctime > SETTINGS["newFilesTimestamp"]:
+            if stats.st_ctime > time_stamp:
                 mask += 4
             if mask:
                 output.append("["+str(mask)+"]" + f)
-    SETTINGS["newFilesTimestamp"] = time.time()
+    SETTINGS["newFilesTimestamp"][start] = time.time()
     send_msg("newFiles", ("\n".join(output)).encode('ascii', 'ignore').decode('ascii'))
     return
